9faf111e658154dfc6094900bd516af1981e282d
Rodrigo Aedo <rodrigo.iac87@gmail.com>
2025-06-14 23:15:27 -0400

    testforoCurso actualizado

--- src/test/java/com/edutech/msforos/service/ForoCursoTest.java
+++ src/test/java/com/edutech/msforos/service/ForoCursoTest.java
@@ -1,30 +1,32 @@
-package com.edutech.msforos.test;
+package com.edutech.msforos.service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.edutech.msforos.model.ForoCurso;
 import com.edutech.msforos.model.MensajeForo;
 import com.edutech.msforos.model.TemaForo;
 import com.edutech.msforos.repository.ForoRepository;
-import com.edutech.msforos.service.ForoService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -36,16 +38,14 @@ import java.time.LocalDate;
 
 public class ForoCursoTest {
 
+    @Captor
+    ArgumentCaptor<ForoCurso> foroCursoCaptor;
+
     @Mock
     private ForoRepository foroRepository;
 
     @InjectMocks
     private ForoService foroService;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
 
     @Test
     void agregarForo_debeGuardarForoyLuegoRetornarForo() {
@@ -66,11 +66,14 @@ public class ForoCursoTest {
         when(foroRepository.save(any(ForoCurso.class))).thenReturn(foroCurso);
 
         ForoCurso resultado = foroService.save(foroCurso);
-
         assertNotNull(resultado);
-        assertEquals("Programacion", resultado.getTitulo());
         assertEquals(1, resultado.getMensajes().size());
-        verify(foroRepository).save(resultado);
+        verify(foroRepository, times(1)).save(foroCursoCaptor.capture());
+
+        ForoCurso capturado = foroCursoCaptor.getValue();
+        assertEquals("Programacion", capturado.getTitulo());
+        assertEquals(1, capturado.getMensajes().size());
+        assertEquals("La programacion", capturado.getMensajes().get(0).getTitulo());
     }
 
     @Test
@@ -91,6 +94,7 @@ public class ForoCursoTest {
 
     @Test
     void eliminarUnforoYdebellamaralRepository() {
+        when(foroRepository.findById(1)).thenReturn(Optional.of(new ForoCurso()));
         foroService.deleteById(1);
         verify(foroRepository).findById(1);
     }
@@ -102,7 +106,7 @@ public class ForoCursoTest {
         ForoCurso nuevoForo = new ForoCurso(0, 1, true, "Programacion orientada a objetos", "Programacion con java",
                 new ArrayList<>());
 
-        when(foroRepository.findById(0)).thenReturn(Optional.of(foroExistente));
+        when(foroRepository.findById(eq(0))).thenReturn(Optional.of(foroExistente));
         when(foroRepository.save(any(ForoCurso.class))).thenReturn(foroExistente);
 
         boolean resultado = foroService.update(0, nuevoForo);
@@ -125,4 +129,49 @@ public class ForoCursoTest {
         verify(foroRepository, never()).save(any());
     }
 
+    @Test
+    void buscarPorIdYretornarElObjeto() {
+
+        ForoCurso foroCurso = new ForoCurso(2, 2, false, "El quijote", "Ciencia ficcion", new ArrayList<>());
+
+        lenient().doReturn(Optional.of(foroCurso)).when(foroRepository).findById(2);
+
+        Optional<ForoCurso> resultado = foroService.findById(2);
+
+        assertTrue(resultado.isPresent());
+        assertEquals(2, resultado.get().getIdForo());
+
+        verify(foroRepository).findById(2);
+    }
+
+    @Test
+    void ActivarForoCursoYretonarTrue() {
+
+        ForoCurso foroCurso = new ForoCurso();
+
+        foroCurso.setHabilitado(false);
+        foroCurso.deshabilitadoCurso();
+        assertFalse(foroCurso.isHabilitado());
+
+        when(foroRepository.save(any(ForoCurso.class))).thenReturn(foroCurso);
+        foroService.save(foroCurso);
+
+        verify(foroRepository).save(foroCursoCaptor.capture());
+        assertEquals(foroCurso, foroCursoCaptor.getValue());
+    }
+
+    @Test
+    void DesactivarCursoYretornarFalse() {
+        ForoCurso foroCurso = new ForoCurso();
+
+        foroCurso.setHabilitado(true);
+        foroCurso.deshabilitadoCurso();
+        assertFalse(foroCurso.isHabilitado());
+
+        when(foroRepository.save(any(ForoCurso.class))).thenReturn(foroCurso);
+        foroService.save(foroCurso);
+        verify(foroRepository).save(foroCursoCaptor.capture());
+        assertEquals(foroCurso, foroCursoCaptor.getValue());
+
+    }
 }
